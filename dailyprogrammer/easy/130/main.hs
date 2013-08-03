b12b089d5d0ace56884f4781e484587b12e2bfd7
Quentin Lambert <daiwen@dualcorn.com>
2013-08-03 10:57:57 +0100

    further cleaning
    modified:   main.hs
    	modified: main
    		using mapM and point free notation

--- dailyprogrammer/easy/130/main.hs
+++ dailyprogrammer/easy/130/main.hs
@@ -11,8 +11,9 @@ import System.Random
 main :: IO()
 main = forever $ do
   dices <- getLine
-  s <- liftM unlines $ sequence $ map (\a -> case a of 
-                                          Left err ->  return err :: IO String
-                                          Right val -> liftM show $ evalRandIO $ ndmDices val) 
-                                      $ map readNDM $ words dices
+  s <- liftM unlines $ mapM ((\a -> case a of 
+                                Left err ->  return err :: IO String
+                                Right val -> liftM show $ evalRandIO $ ndmDices val) 
+                             . readNDM) $ words dices
+       
   putStr s
