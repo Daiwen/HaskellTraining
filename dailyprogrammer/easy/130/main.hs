0f9d1cb863d770e46d6fd32c318090c0bc167d0f
Quentin Lambert <daiwen@dualcorn.com>
2013-08-03 10:26:51 +0100

    cleaning main.hs
    modified:   main.hs
    	deleted: myUnlines
    	modified: main
    		using sequence, liftM and unlines

--- dailyprogrammer/easy/130/main.hs
+++ dailyprogrammer/easy/130/main.hs
@@ -11,13 +11,8 @@ import System.Random
 main :: IO()
 main = forever $ do
   dices <- getLine
-  s <- myUnlines $ map (\a -> case a of 
-                           Left err ->  Left err 
-                           Right val -> Right $ liftM show $ evalRandIO $ ndmDices val) 
-                       $ map readNDM $ words dices
+  s <- liftM unlines $ sequence $ map (\a -> case a of 
+                                          Left err ->  return err :: IO String
+                                          Right val -> liftM show $ evalRandIO $ ndmDices val) 
+                                      $ map readNDM $ words dices
   putStr s
-  
-myUnlines :: [Either String (IO String)] -> IO String
-myUnlines [] = return []
-myUnlines ((Left a):xs) = liftM2 (++) (return $ a ++ "\n") $ myUnlines xs
-myUnlines ((Right a):xs) = liftM2 (\x y ->x ++ "\n" ++ y) a $ myUnlines xs
